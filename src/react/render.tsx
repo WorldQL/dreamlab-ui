d6ae31b01e7de4f8be3887d463c8374b4260be32
Jack Baron <jackmbaron@gmail.com>
2023-11-06 14:22:30 +0000

    allow passing in a container element

--- src/react/render.tsx
+++ src/react/render.tsx
@@ -3,10 +3,14 @@ import { createRoot } from 'https://esm.sh/react-dom@18.2.0/client'
 import type { ReactNode } from 'https://esm.sh/react@18.2.0'
 import { GameContext } from '~/react/context.ts'
 
-export const renderUI = (game: Game<false>, ui: ReactNode) => {
-  const container = document.createElement('div')
-  game.client.ui.add(container)
+export const renderUI = (
+  game: Game<false>,
+  ui: ReactNode,
+  container?: HTMLDivElement,
+) => {
+  const div = container ?? document.createElement('div')
+  game.client.ui.add(div)
 
-  const root = createRoot(container)
+  const root = createRoot(div)
   root.render(<GameContext.Provider value={game}>{ui}</GameContext.Provider>)
 }
