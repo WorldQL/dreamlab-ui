8669a469a7f400453d5de0d8ae26d5ec19383f72
Jack Baron <jackmbaron@gmail.com>
2023-11-06 16:00:11 +0000

    doc

--- src/react/hooks/useForceUpdate.ts
+++ src/react/hooks/useForceUpdate.ts
@@ -1,5 +1,10 @@
 import { useCallback, useState } from 'https://esm.sh/react@18.2.0'
 
+/**
+ * Allows you to force a re-render
+ *
+ * Useful for tracking game state and forcing the UI to react to events
+ */
 export const useForceUpdate = () => {
   const [, updateState] = useState<unknown>()
   return useCallback(() => updateState({}), [])
