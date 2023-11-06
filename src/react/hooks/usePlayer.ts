7873e631474beaf7e16196b4b3013e5ea923c61b
Jack Baron <jackmbaron@gmail.com>
2023-11-06 15:57:21 +0000

    force a re-render when player entity changes

--- src/react/hooks/usePlayer.ts
+++ src/react/hooks/usePlayer.ts
@@ -1,8 +1,32 @@
 import { isPlayer } from '@dreamlab.gg/core/entities'
 import type { Player } from '@dreamlab.gg/core/entities'
+import type { EventHandler } from '@dreamlab.gg/core/events'
+import { useCallback, useEffect, useState } from 'https://esm.sh/react@18.2.0'
 import { useGame } from './useGame.ts'
 
 export const usePlayer = (): Player | undefined => {
   const game = useGame()
+
+  const [, updateState] = useState<unknown>()
+  const forceUpdate = useCallback(() => updateState({}), [])
+
+  type Handler = EventHandler<'onDestroy' | 'onInstantiate'>
+  const onEntityChange = useCallback<Handler>(
+    entity => {
+      if (isPlayer(entity)) forceUpdate()
+    },
+    [forceUpdate],
+  )
+
+  useEffect(() => {
+    game.events.common.addListener('onInstantiate', onEntityChange)
+    game.events.common.addListener('onDestroy', onEntityChange)
+
+    return () => {
+      game.events.common.removeListener('onInstantiate', onEntityChange)
+      game.events.common.removeListener('onDestroy', onEntityChange)
+    }
+  }, [game.events.common, onEntityChange])
+
   return game.entities.find(isPlayer)
 }
