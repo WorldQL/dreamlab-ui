086eba030ffe67e07b63b9b96e60e1fcf9415abe
Jack Baron <jackmbaron@gmail.com>
2023-11-09 14:04:18 +0000

    hook to list registered spawnable entities

---
+++ src/react/hooks/useRegistered.ts
@@ -0,0 +1,22 @@
+import type { EventHandler } from '@dreamlab.gg/core/events'
+import { useCallback, useEffect } from 'https://esm.sh/react@18.2.0'
+import { useForceUpdate } from './useForceUpdate.ts'
+import { useGame } from './useGame.ts'
+
+export const useRegistered = () => {
+  const game = useGame()
+  const forceUpdate = useForceUpdate()
+
+  type Handler = EventHandler<'onRegister'>
+  const onRegister = useCallback<Handler>(() => forceUpdate(), [forceUpdate])
+
+  useEffect(() => {
+    game.events.common.addListener('onRegister', onRegister)
+
+    return () => {
+      game.events.common.removeListener('onRegister', onRegister)
+    }
+  }, [game.events.common, onRegister])
+
+  return game.registered
+}
