bc3576e9a1827296d1ea337ac21b866977586c66
Jack Baron <jackmbaron@gmail.com>
2023-11-09 15:19:49 +0000

    strongly type

--- src/react/hooks/useRegistered.ts
+++ src/react/hooks/useRegistered.ts
@@ -1,9 +1,13 @@
+import type { BareSpawnableFunction } from '@dreamlab.gg/core'
 import type { EventHandler } from '@dreamlab.gg/core/events'
 import { useCallback, useEffect } from 'https://esm.sh/react@18.2.0'
 import { useForceUpdate } from './useForceUpdate.ts'
 import { useGame } from './useGame.ts'
 
-export const useRegistered = () => {
+export const useRegistered = (): readonly (readonly [
+  name: string,
+  fn: BareSpawnableFunction,
+])[] => {
   const game = useGame()
   const forceUpdate = useForceUpdate()
 
