ca2423defaf2752bd70c434592cd04f5413b0973
Jack Baron <jackmbaron@gmail.com>
2023-11-09 13:25:08 +0000

    fix breaking

--- src/react/hooks/inputs.ts
+++ src/react/hooks/inputs.ts
@@ -1,14 +1,14 @@
-import type { KeyCode } from '@dreamlab.gg/core/input'
+import type { InputCode } from '@dreamlab.gg/core/input'
 import { useCallback, useEffect } from 'https://esm.sh/react@18.2.0'
 import type { LiteralUnion } from 'type-fest'
 import { useGame } from './useGame.ts'
 
-type Input = LiteralUnion<KeyCode, string>
+type KeyOrInput = LiteralUnion<InputCode, string>
 export type InputHandler = (pressed: boolean) => void
 export type InputPressedHandler = () => void
 
 export const useInput = (
-  input: Input,
+  input: KeyOrInput,
   handler: (pressed: boolean) => void,
 ): void => {
   const game = useGame()
@@ -22,7 +22,7 @@ export const useInput = (
   }, [game, input, handler])
 }
 
-export const useInputPressed = (input: Input, handler: () => void): void => {
+export const useInputPressed = (input: KeyOrInput, handler: () => void): void => {
   const game = useGame()
 
   const fn = useCallback(
